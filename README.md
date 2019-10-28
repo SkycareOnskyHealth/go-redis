381bb6d6035be91a1d5eb885c4b6da5fad43d555
Phạm Nguyên Cát <dtpham258@gmail.com>
2019-10-28 19:07:04 +0700

    update docs

--- README.md
+++ README.md
@@ -1,22 +1,60 @@
-# Resource Helper
+# Go-redis Helper
 
-[![N|Solid](https://cldup.com/dTxpPi9lDf.thumb.png)](https://github.com/nguyencatpham/request-handler)
+[![N|Solid](https://cldup.com/dTxpPi9lDf.thumb.png)](https://github.com/onskycloud/go-redis)
 
-[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://github.com/nguyencatpham/request-handler)
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://github.com/onskycloud/go-redis)
 
-Resource helper support for parser data from resource service.
+Redis client for golang
 
 ## Getting Started!
 
+> Get by key
   ```
-    Options{
-        Addr:     "localhost:6379",
-        Password: "", // no password set
-        DB:       0,  // use default DB
-     }
-    client = redis.NewRedis(&options)
+  func (r *Redis) Get(key string) (string, error) {
+    return r.db.Get(key).Result()
+  }
   ```
-
+> Get by key and field
+  ```
+  func (r *Redis) GetObject(objectKey string, field string, result interface{}) error {
+    temp, err := r.db.HGet(objectKey, field).Bytes()
+    if err != nil {
+      return err
+    }
+    err = msgpack.Unmarshal(temp, result)
+    return err
+  }
+  ```
+> Set by key
+  ```
+  func (r *Redis) Set(key string, value interface{}, expiration time.Duration) (string, error) {
+	return r.db.Set(key, value, expiration).Result()
+  }
+  ```
+> Set by key and field
+  ```
+  func (r *Redis) SetObject(objectKey string, field string, value interface{}) (bool, error) {
+	bytes, err := msgpack.Marshal(value)
+	if err != nil {
+		return false, err
+	}
+	return r.db.HSet(objectKey, field, bytes).Result()
+  }
+  ```
+> Check exist by key and field
+  ```
+  // CheckExistedObject will return true if the object is existed.
+  func (r *Redis) CheckExistedObject(objectKey string, field string) (bool, error) {
+    existed, err := r.db.HExists(objectKey, field).Result()
+    if err != nil {
+      return false, err
+    }
+    if existed == true {
+      return true, nil
+    }
+    return false, nil
+  }
+  ```
 ### Installation
 
 Request handler requires [Go](https://golang.org/) v1.13+ to run.
--- redis.go
+++ redis.go
@@ -1,49 +1,67 @@
 package redis
 
 import (
-	"github.com/go-redis/redis"
-	"github.com/vmihailenco/msgpack"
 	"time"
+
+	"github.com/go-redis/redis"
+	"github.com/onskycloud/structs"
+	"github.com/vmihailenco/msgpack"
 )
 
+// NewRedis instance
 func NewRedis(options interface{}) *Redis {
 	opts := new(redis.Options)
-	Merge(opts, options)
+	structs.Merge(opts, options)
 	client := redis.NewClient(opts)
 	return &Redis{
 		db: client,
 	}
 }
 
+// Redis struct
 type Redis struct {
 	db       *redis.Client
 	pipeline *redis.Pipeline
 }
 
+// DB constructor
 func (r *Redis) DB() *redis.Client {
 	return r.db
 }
 
+// Close close redis
 func (r *Redis) Close() error {
 	return r.db.Close()
 }
+
+// Ping ping
 func (r *Redis) Ping() *redis.StatusCmd {
 	return r.db.Ping()
 }
+
+// Del del object by keys
 func (r *Redis) Del(keys ...string) error {
 	_, err := r.db.Del(keys...).Result()
 	return err
 }
+
+// HDel del object by key and field
 func (r *Redis) HDel(key string, fields ...string) error {
 	_, err := r.db.HDel(key, fields...).Result()
 	return err
 }
+
+// Get get object by key
 func (r *Redis) Get(key string) (string, error) {
 	return r.db.Get(key).Result()
 }
+
+// Set set object by key
 func (r *Redis) Set(key string, value interface{}, expiration time.Duration) (string, error) {
 	return r.db.Set(key, value, expiration).Result()
 }
+
+// SetObject set object by key and field
 func (r *Redis) SetObject(objectKey string, field string, value interface{}) (bool, error) {
 	bytes, err := msgpack.Marshal(value)
 	if err != nil {
@@ -51,6 +69,8 @@ func (r *Redis) SetObject(objectKey string, field string, value interface{}) (bo
 	}
 	return r.db.HSet(objectKey, field, bytes).Result()
 }
+
+// GetObject get object by key and field
 func (r *Redis) GetObject(objectKey string, field string, result interface{}) error {
 	temp, err := r.db.HGet(objectKey, field).Bytes()
 	if err != nil {
@@ -66,8 +86,8 @@ func (r *Redis) CheckExistedObject(objectKey string, field string) (bool, error)
 	if err != nil {
 		return false, err
 	}
-	if existed == true {
-		return true, nil
+	if !existed {
+		return false, nil
 	}
-	return false, nil
+	return true, nil
 }
